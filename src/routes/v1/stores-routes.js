b2df4868c046f97260f430179ba4cc3566ba4abc
jconnor0078 <jconnor0078@hotmail.com>
2022-03-11 15:02:35 -0500

    subiendo cambio

--- src/routes/v1/stores-routes.js
+++ src/routes/v1/stores-routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/create', storesController.createStore);
 router.post('/update', storesController.updateStore);
-router.post('/delete/:storeId', storesController.deleteStore);
+router.get('/delete/:storeId', storesController.deleteStore);
 router.get('/get-all', storesController.getStores);
 router.get('/get-by-id/:storeId', storesController.getStoreById);
 
